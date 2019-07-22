51ba20442bf41ac2df114b10770229a7da263e80
Charles Blake <charlechaud@gmail.com>
2019-07-22 11:21:57 -0400

    Try to get auto-versioning working.

--- lc.nim
+++ lc.nim
@@ -59,7 +59,8 @@ type       # fileName Dtype Stat lnTgt ACL Magic Capability
     when defined(lcMagic): mc: magic_t
 
 ###### Documentation/CLI; Early to use lsCfFromCL in for local config tweaks.
-clCfg.version = "version 1.0"
+const vsn = staticExec "git describe --tags HEAD"
+clCfg.version = vsn
 let cfDfl* = LsCf(format:"%f", glyph:" -> ", recurse:1, nColumn:999, padMax:999)
 
 initGen(cfDfl, LsCf, "paths", @["ALL AFTER paths"], "inLsCf")
