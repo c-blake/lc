a322e1758dc37a61b20679530cf24e4bd6599238
Charles Blake <charlechaud@gmail.com>
2023-07-19 14:23:19 -0400

    Show a few ideas for using command format extensions at the bottom. With the current API (as long said in the comments defining ExtFmt & ExtType) managing call-to-call state globally within the library would obviously be better example code than running a command per file.
    Maybe a good demo of this might be libcoproc?  Fire up a kid and send
    requests-receive replies over a pair of pipes, keeping separated memory.
    This would at least nicely show all heavyweight/start-up work done once
    per `lc` invocation super explicitly, with just a fast write-read cycle
    per file-request on either side (plus whatever work is entailed in
    computing a reply..).  That might put pressure on some kind of async,
    though, which is always more messy.  Maybe any student advanced enough
    to worry about sync slowdowns can grow well out of the example.  Hmm..

--- extensions/lcNim.nim
+++ extensions/lcNim.nim
@@ -12,12 +12,16 @@ proc cmdExitsOk(cmd: string, qualPath: cstring): cint =
   (execShellCmd(cmd & " " & qp) == 0).cint
 
 proc te1(qualPath: cstring): cint {.noconv, exportc, dynlib.} =
-  cmdExitsOk("te1", qualPath)  # te == T)est E)xtension
+  cmdExitsOk("te1", qualPath) # te == T)est E)xtension
 
 proc te2(qualPath: cstring): cint {.noconv, exportc, dynlib.} =
   cmdExitsOk("te2", qualPath)
 
-# With this part, lc -e liblcNim.so:fe1 -f'%e %f' does something interesting.
+# With this part, lc -e liblcNim.so:fe1 -f'%e %f' does something interesting if
+# a program in $PATH named "fe1" also does something interesting (when given a
+# fully qualified path as $1) and similarly for "fe2" in both places. { It may
+# be helpful to move to a 5-field param: lib:hdr:ini:arg:fun & call ob=ini(arg)
+# just once and later fun(ob, qpath) for each file to avoid global state. }
 var res: string               # Use a global to avoid after-call GC
 proc cmdOutput(cmd: string, qualPath: cstring): cstring =
   qpString(qualPath)
@@ -30,7 +34,22 @@ proc cmdOutput(cmd: string, qualPath: cstring): cstring =
     res.setLen(0)
 
 proc fe1(qualPath: cstring): cstring {.noconv, exportc, dynlib.} =
-  cmdOutput("fe1", qualPath)   # fe == F)ormat E)xtension
+  cmdOutput("fe1", qualPath)  # fe == F)ormat E)xtension
 
 proc fe2(qualPath: cstring): cstring {.noconv, exportc, dynlib.} =
   cmdOutput("fe2", qualPath)
+
+# 3 ideas to get you started: space usage, security contexts, git status
+# #!/bin/sh
+# du -s "$1" | awk '{print $1}'
+# #!/bin/sh
+# ls -1Z "$FILE_NAME" | awk '{print $1}' | tr -d \\n
+# #!/bin/sh
+# d="${1%/*}"; b="${1##*/}"         # d)ir & b)asename
+# n="/dev/null"; t="$(printf '\t')" # n)ull & t)ab
+# case $(cd "$d" 2>$n; git status 2>$n |noc| grep "$b") in
+#   "${t}new file: "*) echo 'A';; # Codes used by Mercurial
+#   "${t}modified: "*) echo 'M';; # Slow, buggy, incomplete
+#   "${t}deleted: "*)  echo 'D';; # More teaser than answer
+#   *?*) echo '?';; *) printf '-' ;;
+# esac
